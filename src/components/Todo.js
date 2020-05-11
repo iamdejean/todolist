7e36a9dcc06e43f2a2f5c211195e70adba4e732a
Jean Japheth Ezekiel <eljean80@gmail.com>
2020-05-11 14:45:00 +0100

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import Todo from './components/Todo';
 
 class Todo extends Component {
-render(){
-regards (
-<div>
-<h1>Todo</h1>
-</div>
-);
-}
+ render(){
+   regards (
+    <div>
+     <h1>Todo</h1>
+    </div>
+   );
+ }
 }
 
 export default Todo;
