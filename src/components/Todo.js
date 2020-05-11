ddeb5382b201d2f76601750825c36e712cf7a202
Jean Japheth Ezekiel <eljean80@gmail.com>
2020-05-11 15:22:40 +0100

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,7 +4,7 @@ import Todoitem from './Todoitem';
 class Todo extends Component {
  render(){
    return this.props.todo.map((todo)=> (
-     <h3>{ todo.title}</h3>
+     <Todoitem todo={todo} />
    ));
  }
 }
