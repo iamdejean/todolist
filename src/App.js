cbbef3223aa46ef046c5e8c736af8b647d4eea55
Jean Japheth Ezekiel <eljean80@gmail.com>
2020-05-11 14:51:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,26 @@ import Todo from './components/Todo'/;
 import './App.css';
 
 class App extends Component {
+ state = {
+  todo: [
+   {
+     id: 1,
+     title: 'Take out the trash',
+     completed: false
+   },
+   {
+     id: 2,
+     title: 'Dinner with Bae',
+     completed: false
+   },
+
+   {
+     id: 3,
+     title: 'Meeting with Boss',
+     completed: false
+   }
+  ]
+ }
  render(){
   return (
     <div className="App">    
